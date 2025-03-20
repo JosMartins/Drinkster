41a147f34edba6d2b213616301bb75abf7e23847
José Martins <josemmartins002@gmail.com>
2025-03-20 16:27:03 +0000

    Refactor difficulty dialog component to use a structured difficulty object and improve validation logic

--- frontend/src/app/difficulty-dialog/difficulty-dialog.component.ts
+++ frontend/src/app/difficulty-dialog/difficulty-dialog.component.ts
@@ -8,21 +8,57 @@ import {Difficulty} from "../difficulty";
 
 @Component({
   selector: 'app-difficulty-dialog',
+  standalone: true,
   templateUrl: './difficulty-dialog.component.html',
-  styleUrls: ['./difficulty-dialog.component.css']
+  styleUrls: ['./difficulty-dialog.component.css'],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, CommonModule]
 })
+
 export class DifficultyDialogComponent {
-  sliderValues: { [key: string]: number } = {
+  difficulty: Difficulty;
+  sliderValues = {
     easy: 33,
     medium: 33,
     hard: 34,
     extreme: 0
   };
-  extremeMode: boolean = false;
-  errorMsg: string | null = null;
+  extremeMode: boolean;
+  errorMsg: string = '';
+
+  constructor(
+    public dialogRef: MatDialogRef<DifficultyDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data : any
+  ) {
+    
+    this.difficulty = {
+      easy: data.difficulty.easy * 100,
+      medium: data.difficulty.medium * 100,
+      hard: data.difficulty.hard * 100,
+      extreme: data.difficulty.extreme * 100
+    };
+    this.extremeMode = data.extremeMode;
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onSave(): void {
+    if (this.difficulty.easy + this.difficulty.medium + this.difficulty.hard + (this.extremeMode ? this.difficulty.extreme : 0) != 100) {
+      this.errorMsg = 'The sum of the difficulties must be equal to 100%!'
+    } else {
+      this.dialogRef.close({
+            easy: this.difficulty.easy / 100,
+            medium: this.difficulty.medium / 100,
+            hard: this.difficulty.hard / 100,
+            extreme: this.extremeMode ? this.difficulty.extreme / 100 : 0
+          });
+    }
+
+  }
 
   onSliderChange(changedSlider: string) {
-    const sliders = this.extremeMode
+    const sliders: (keyof typeof this.sliderValues)[] = this.extremeMode
       ? ['easy', 'medium', 'hard', 'extreme']
       : ['easy', 'medium', 'hard'];
 
@@ -40,6 +76,6 @@ export class DifficultyDialogComponent {
       }
     }
 
-    this.errorMsg = total !== 100 ? 'The total must be 100%' : null;
+    this.errorMsg = total !== 100 ? 'The total must be 100%' : '';
   }
 }
