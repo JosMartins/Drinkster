0631268c568e8648db366eb9e7ecf4de7568631f
José Martins <josemmartins002@gmail.com>
2025-03-21 01:00:34 +0000

    Refactor singleplayer component to enhance player management; update player model to include difficulty settings and adjust UI for better accessibility.

--- frontend/src/app/singleplayer/singleplayer.component.ts
+++ frontend/src/app/singleplayer/singleplayer.component.ts
@@ -2,14 +2,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgFor } from "@angular/common";
 import { Player } from '../player';
-import { Difficulty } from "../difficulty";
+import { Difficulty , DEFAULT_DIFFICULTY } from "../difficulty";
 import { MatDialog } from '@angular/material/dialog';
 import { DifficultyDialogComponent } from '../difficulty-dialog/difficulty-dialog.component';
 import {Router} from "@angular/router";
-import { io } from 'socket.io-client';
 
-
-const DEFAULT_DIFFICULTY: Difficulty = { easy: 0.30, medium: 0.35, hard:0.35, extreme: 0};
 
 
 @Component({
@@ -24,14 +21,13 @@ export class SingleplayerComponent {
 
   constructor(public dialog: MatDialog, private readonly router: Router) { }
 
-  players: Player[] = [{ name: '', gender: 'M'}];
-  difficultyValues : Difficulty = DEFAULT_DIFFICULTY;
+  players: Player[] = [{ name: '', gender: 'M', difficulty: DEFAULT_DIFFICULTY }];
   numberOfRememberedChal: number = 20
   randomMode: boolean = false;
 
 
   addPlayer() {
-    this.players.push({ name: '', gender: 'M'});
+    this.players.push({ name: '', gender: 'M', difficulty: DEFAULT_DIFFICULTY });
   }
 
   removePlayer(index: number) {
@@ -40,6 +36,11 @@ export class SingleplayerComponent {
     }
   }
 
+  changeDifficulty(index: number) {
+    this.openDifficultyModal(index);
+
+
+  }
 
   startGame() {
     //TODO
@@ -51,17 +52,17 @@ export class SingleplayerComponent {
   toggleRandomMode() {
     this.randomMode = !this.randomMode;
   }
-  openDifficultyModal() {
+  openDifficultyModal(index: number) {
 
     const dialogRef = this.dialog.open(DifficultyDialogComponent, {
       width: '350px',
-      data: {difficulty: this.difficultyValues}
+      data: { difficultyValues: this.players[index].difficulty }
 
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.difficultyValues = result;
+        this.players[index].difficulty = result;
       }
     });
   }
