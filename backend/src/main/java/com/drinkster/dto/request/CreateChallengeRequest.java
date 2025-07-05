a5e003f79f9b5976ba6780b53a1a284dec7e91e0
José Martins <josemmartins002@gmail.com>
2025-07-05 12:57:34 +0100

    removed unused imports

--- backend/src/main/java/com/drinkster/dto/request/CreateChallengeRequest.java
+++ backend/src/main/java/com/drinkster/dto/request/CreateChallengeRequest.java
@@ -1,10 +1,11 @@
 package com.drinkster.dto.request;
 
 
-import jakarta.validation.Valid;
+
+import java.util.Arrays;
+
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CreateChallengeRequest (
